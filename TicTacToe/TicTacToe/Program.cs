d1e08ab4e8c3034d6008a43053e5b0edaa26f2af
Marshall Ferguson <moferg01@louisville.edu>
2018-04-25 14:54:27 -0400

    Added requirements outline

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -10,6 +10,19 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
+            // Requirements:
+            // Console based (no GUI)
+            // Two players take turns using same keyboard
+            // Player designates which square they want to play using keyboard
+                // Possible approach: number the squares in board with digits 1-9
+                // Ex: If player enters 7, top left corner of board is chosen
+            // Game should ensure only legal moves are made
+                // Cannot play a square that already has something
+                // Wrong player cannot take a turn
+                // If player makes illegal move, board should remain unchanged
+            // Game should be able to detect when a player wins or when a draw happens (board is full)
+            // When game is over, outcome is displayed to user
+            // Only a single round of play is required
         }
     }
 }
