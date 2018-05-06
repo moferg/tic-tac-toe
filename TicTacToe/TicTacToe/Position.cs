b81e54e432d90fd6e20179448dba028889bca7a8
Marshall Ferguson <moferg01@louisville.edu>
2018-05-06 15:08:14 -0400

    Added and outlined Position class

---
+++ TicTacToe/TicTacToe/Position.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Position // Used to represent a location on the board
+    {
+        // Row Property
+        // Column Property
+    }
+}
