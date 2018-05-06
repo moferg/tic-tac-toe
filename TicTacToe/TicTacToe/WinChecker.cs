cbe040a2d8ce3cb655c54100f54fc60e119c0d31
Marshall Ferguson <moferg01@louisville.edu>
2018-05-06 15:25:37 -0400

    Added and outlined WinChecker class

---
+++ TicTacToe/TicTacToe/WinChecker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class WinChecker // Analyzes board and checks if player has won or not
+    {
+        // Method outlines
+        // public State Check(Board currentState)
+        // public bool IsDraw(Board currentState)
+    }
+}
