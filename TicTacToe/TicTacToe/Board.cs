1f88e7eca25d2e9bdfb31b2f738a99179a8ca9f3
Marshall Ferguson <moferg01@louisville.edu>
2018-05-06 15:15:51 -0400

    Added and outlined Board class

---
+++ TicTacToe/TicTacToe/Board.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Board // Central DS for game as whole
+    {
+        // 3X3 array of State values to represent board
+        // Ways to request current state of position on board
+        // Ways to request placing a new value on the board
+        // Ways to keep track of whose turn it is
+        // Ways to make sure only correct player can take turn
+    }
+}
