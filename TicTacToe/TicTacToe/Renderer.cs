add8c452d35237840ae2d5338a33c046ae5da501
Marshall Ferguson <moferg01@louisville.edu>
2018-05-06 15:32:18 -0400

    Added and outlined Renderer class

---
+++ TicTacToe/TicTacToe/Renderer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Renderer 
+    {
+        // Draws game's current state
+        // Displays final results when the game is over
+    }
+}
