775405c892084601b262452bba9b6e789598129d
Marshall Ferguson <moferg01@louisville.edu>
2018-05-06 15:21:19 -0400

    Added and oultined Player class

---
+++ TicTacToe/TicTacToe/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Player // Tranlates user input from player into a board position to play on
+    {
+        // Method outline
+        // public Position GetPosition(Board currentState)
+    }
+}
