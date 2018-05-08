9a05dd1832e63325598f66538fb1f262c63566df
Marshall Ferguson <moferg01@louisville.edu>
2018-05-08 13:18:42 -0400

    Coded Renderer class

--- TicTacToe/TicTacToe/Renderer.cs
+++ TicTacToe/TicTacToe/Renderer.cs
@@ -6,9 +6,44 @@ using System.Threading.Tasks;
 
 namespace TicTacToe
 {
-    class Renderer 
+    public class Renderer 
     {
-        // Draws game's current state
-        // Displays final results when the game is over
+        public void Render(Board board)
+        {
+            char[,] symbols = new char[3, 3];
+            for (int row = 0; row < 3; row++)
+                for (int column = 0; column < 3; column++)
+                    symbols[row, column] = SymbolFor(board.GetState(new Position(row, column)));
+
+            Console.WriteLine($" {symbols[0, 0]} | {symbols[0, 1]} | {symbols[0, 2]} ");
+            Console.WriteLine("---+---+---");
+            Console.WriteLine($" {symbols[1, 0]} | {symbols[1, 1]} | {symbols[1, 2]} ");
+            Console.WriteLine("---+---+---");
+            Console.WriteLine($" {symbols[2, 0]} | {symbols[2, 1]} | {symbols[2, 2]} ");
+        }
+
+        private char SymbolFor(State state)
+        {
+            switch(state)
+            {
+                case State.O: return 'O';
+                case State.X: return 'X';
+                default: return ' ';
+            }
+        }
+
+        public void RenderResults(State winner)
+        {
+            switch(winner)
+            {
+                case State.O:
+                case State.X:
+                    Console.WriteLine(SymbolFor(winner) + " wins!");
+                    break;
+                case State.Undecided:
+                    Console.WriteLine("Draw!");
+                    break;
+            }
+        }
     }
 }
